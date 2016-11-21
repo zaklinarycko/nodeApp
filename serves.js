9889a9ceb5dcce3f5ac84260fbd64b6d7d1b5226
Zaki <1404039@rgu.ac.uk>
2016-11-21 13:42:00 +0000

    Initial commit

---
+++ serves.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Zaki on 21/11/2016.
+ */
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
